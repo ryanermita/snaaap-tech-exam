cb8c66d44afc64ca48fe44cb23c9798c18243891
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 23:22:52 +0800

    include flassger to requirements and create swagger initializer function.

--- src/initializer.py
+++ src/initializer.py
@@ -1,7 +1,10 @@
 import os
 import logging
 
+from src.utils import common_util
+
 import pymodm
+from flasgger import Swagger
 
 
 def _initialize_db():
@@ -18,4 +21,20 @@ def _initialize_db():
         raise e
     except Exception as e:
         logging.error(f"Unexpected error while initializing database: {e}")
+        raise e
+
+
+def _initialize_swagger(app):
+    try:
+        swagger_enabled = common_util.to_bool(os.environ.get("SWAGGER_ENABLED",
+                                                             False))
+        if not swagger_enabled:
+            return
+
+        return Swagger(app)
+    except KeyError as e:
+        logging.error(f"Missing config: {e}")
+        raise e
+    except Exception as e:
+        logging.error(f"Unexpected error while initializing swagger: {e}")
         raise e
