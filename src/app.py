d60aff06603cdec64912142f8cfd6e783d84d10f
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 23:23:22 +0800

    retrieve host from env var and initialize swagger

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+import os
+
 from user.user_controller import user
 from error_handlers.api_error_handler import api_error_handler
-from initializer import _initialize_db
+import initializer
 
 from flask import Flask
 
@@ -10,6 +12,9 @@ app.register_blueprint(user)
 app.register_blueprint(api_error_handler)
 
 if __name__ == '__main__':
-    _initialize_db()
-    # TODO: retrieve app host from env var
-    app.run(host="0.0.0.0", debug=True)
+    initializer._initialize_db()
+    initializer._initialize_swagger(app)
+
+    app.run(host=os.environ.get('API_HOST', '0.0.0.0'),
+            debug=True,
+            port=os.environ.get('API_PORT', 5000))
