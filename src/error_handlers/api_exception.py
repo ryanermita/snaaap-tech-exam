767aa21c8a8149c9abcd3da313e032d564155a16
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 15:23:08 +0800

    create RequestParameException.

--- src/error_handlers/api_exception.py
+++ src/error_handlers/api_exception.py
@@ -2,3 +2,8 @@ class MissingRequiredFields(Exception):
 
     def __init__(self, error_data):
         self.error_data = error_data
+
+
+class RequestParamException(Exception):
+    def __init__(self, error_data):
+        self.error_data = error_data
