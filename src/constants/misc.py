59842f7c14a14893eabe22473f27deadce445b2d
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 15:25:15 +0800

    create misc constants to store app constants.

---
+++ src/constants/misc.py
@@ -0,0 +1,10 @@
+class GenericConstants:
+    MINIMUM_STRING_LENGTH = 1
+    MAXIMUM_STRING_LENGTH = 255
+    ID_LENGTH = 24
+
+
+class UserConstants:
+    USER_TYPE_USER = 'user'
+    USER_TYPE_ORG = 'org'
+    USER_TYPES = (USER_TYPE_USER, USER_TYPE_ORG)
