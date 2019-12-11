373b4f43c68189fc07b656cba9d5128060921165
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 17:49:15 +0800

    create ResourceNotFound api exception

--- src/error_handlers/api_exception.py
+++ src/error_handlers/api_exception.py
@@ -7,3 +7,8 @@ class MissingRequiredFields(Exception):
 class RequestParamException(Exception):
     def __init__(self, error_data):
         self.error_data = error_data
+
+
+class ResourceNotFound(Exception):
+    def __init__(self, error_data):
+        self.error_data = error_data
