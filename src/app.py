532c3f85410a3846cc72818ecd03aae4fe060678
Ryan Ermita <ryanermita.dev@gmail.com>
2019-12-11 16:14:23 +0800

    create api error handlers.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from user.user_controller import user
+from error_handlers.api_error_handler import api_error_handler
 from initializer import _initialize_db
 
 from flask import Flask
@@ -6,6 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.register_blueprint(user)
+app.register_blueprint(api_error_handler)
 
 if __name__ == '__main__':
     _initialize_db()
--- src/error_handlers/api_error_handler.py
+++ src/error_handlers/api_error_handler.py
@@ -1,4 +1,5 @@
-from src.exceptions.generic import MissingRequiredFields
+from src.error_handlers.api_exception import (MissingRequiredFields,
+                                              RequestParamException)
 from src.constants import http_status_code
 
 from flask import Blueprint, jsonify
@@ -15,3 +16,23 @@ def missing_required_fields(error):
         "results": []
     }
     return jsonify(response), http_status_code.UNPROCESSABLE_ENTITY
+
+
+@api_error_handler.app_errorhandler(RequestParamException)
+def request_param_exception(error):
+    response = {
+        "success": False,
+        "errors": error.args[0],
+        "results": []
+    }
+    return jsonify(response), http_status_code.UNPROCESSABLE_ENTITY
+
+
+@api_error_handler.app_errorhandler(Exception)
+def internal_server_error(error):
+    response = {
+        "success": False,
+        "errors": "Something went wrong.",
+        "results": []
+    }
+    return jsonify(response), http_status_code.INTERNAL_SERVER_ERROR
